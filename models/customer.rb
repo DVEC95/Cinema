005490a33d9caaf064a8151a6adb572cf37771fe
David Eastcroft <davideastcroft95@gmail.com>
2020-03-28 18:00:17 +0000

    Refactors ticket method.

--- models/customer.rb
+++ models/customer.rb
@@ -37,10 +37,7 @@ class Customer
   end
 
   def tickets()
-    sql = 'SELECT films.* FROM films INNER JOIN tickets ON films.id = tickets.film_id WHERE customer_id = $1'
-    values = [@id]
-    film_data = SQLRunner.run(sql, values)
-    return film_data.count
+    return self.films.count
   end
 
   def buy_tickets
