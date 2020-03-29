4ac5956e52fd0380e10926f166b9bc01b6d2d2d6
David Eastcroft <davideastcroft95@gmail.com>
2020-03-29 20:07:07 +0100

    Adds more sample data.

--- console.rb
+++ console.rb
@@ -28,12 +28,45 @@ film1 = Film.new({
   })
 film1.save()
 
+film2 = Film.new({
+  'title' => 'The Good, The Bad & The Ugly',
+  'price' => 5
+  })
+film2.save()
+
+film3 = Film.new({
+  'title' => '2001: A Space Odyssey',
+  'price' => 10
+  })
+film3.save()
+
+film4 = Film.new({
+  'title' => 'Blade Runner',
+  'price' => 10
+  })
+film4.save()
+
 #TICKET DATA --------------
 ticket1 = Ticket.new ({
   'customer_id' => customer1.id,
   'film_id' => film1.id
   })
 ticket1.save()
+
+ticket2 = Ticket.new ({
+  'customer_id' => customer2.id,
+  'film_id' => film1.id
+  })
+ticket2.save()
+
+ticket3 = Ticket.new ({
+  'customer_id' => customer2.id,
+  'film_id' => film2.id
+  })
+ticket3.save()
+
+#BUYING TICKETS
+customer2.buy_tickets()
 
 binding.pry
 nil
